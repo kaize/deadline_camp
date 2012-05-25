095caf36ae5a5743ea98fe2c4c20a097df59d6e6
Mokevnin Kirill <mokevnin@gmail.com>
2012-05-25 18:46:56 +0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeadlineCamp::Application.config.secret_token = '9e5310512eda82cbffedd04a6bb0168a3be1dfa4c083bddc4bedd381cc0c730c2e019a358359dee43a9a565a7f2d7d5e450fa6862178d9b01304b3fd700e9879'
