ba607e6fa4ef76c3e7c1ad0dd722f60802225ede
Mikhail Stolbov <mstolbov@gmail.com>
2012-06-09 15:47:22 +0400

    added production deploy config

---
+++ config/deploy/production.rb
@@ -0,0 +1,12 @@
+set :rails_env, :production
+set :branch do
+  raise "Use tags: TAG=v2" unless ENV['TAG']
+  ENV['TAG']
+end
+set :user, 'deadline_camp_production'
+set :keep_releases, 15
+
+role :web, "62.76.184.142"
+role :app, "62.76.184.142"
+role :db,  "62.76.184.142", :primary => true
+
