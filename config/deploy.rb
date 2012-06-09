06ac1a521f724a962dc150bd3165f9933d15403b
Mikhail Stolbov <mstolbov@gmail.com>
2012-06-09 16:27:00 +0400

    fixed deploy db symlink

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,11 +17,12 @@ default_run_options[:pty] = true
 namespace :deploy do
   desc "Symlinks the database.yml"
   task :symlink_db, :roles => :app do
-    run "ln -nfs #{shared_path}/config/database.yml.simple #{release_path}/config/database.yml"
+    run "ln -nfs #{release_path}/config/database.yml.simple #{release_path}/config/database.yml"
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
   end
 end
 
-after 'deploy:update_code', 'deploy:symlink_db'
+before 'deploy:finalize_update', 'deploy:symlink_db'
 after "deploy:restart", "unicorn:restart"
 after "deploy:update", "deploy:cleanup"
 
