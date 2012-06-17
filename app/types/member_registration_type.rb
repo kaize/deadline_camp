3f611b66132e5a41e323fa4c8446033a52644fd1
Mikhail Stolbov <mstolbov@gmail.com>
2012-06-17 20:49:51 +0400

    added terms validate

---
+++ app/types/member_registration_type.rb
@@ -0,0 +1,11 @@
+class MemberRegistrationType < Member
+  include BaseType
+
+  attr_accessible :additional_educations_attributes, :achievements_attributes, :langs_attributes,
+    :skill_program_langs_attributes, :skill_ides_attributes, :jobs_attributes, :skill_databases_attributes,
+    :skill_operation_systems_attributes, :skill_others_attributes, :others_attributes, :preferences_attributes,
+    :password_confirmation, :terms_of_service
+
+  validates :terms_of_service, :acceptance => true
+
+end
