03013baab900b6065b9c83498c178baf05e3016d
Mikhail Stolbov <mstolbov@gmail.com>
2012-06-19 17:39:42 +0400

    added backup

--- Gemfile
+++ Gemfile
@@ -21,6 +21,7 @@ gem 'cocoon'
 gem 'capi'
 gem 'airbrake'
 gem 'simple_form'
+gem "backup"
 
 # Gems used only for assets and not required
 # in production environments by default.
---
+++ config/backup.rb
@@ -0,0 +1,33 @@
+##
+# Backup
+# Generated Template
+#
+# For more information:
+#
+# View the Git repository at https://github.com/meskyanichi/backup
+# View the Wiki/Documentation at https://github.com/meskyanichi/backup/wiki
+# View the issue log at https://github.com/meskyanichi/backup/issues
+#
+# When you're finished configuring this configuration file,
+# you can run it from the command line by issuing the following command:
+#
+# $ backup perform -t my_backup [-c <path_to_configuration_file>]
+
+Backup::Model.new(:deadline_staging, "Deadline-Camp Backup") do
+  database PostgreSQL do |db|
+    db.name               = "deadline_camp_staging"
+    #db.username           = "kipr"
+    #db.password           = "ASDF43FASDfAasdfADfHTRwerZCXV"
+    #db.host               = "127.0.0.1"
+  end
+
+  compress_with Gzip do |compression|
+    compression.best = true
+    compression.fast = false
+  end
+
+  store_with Local do |local|
+    local.path = "/var/tmp/"
+    local.keep = 5
+  end
+end
