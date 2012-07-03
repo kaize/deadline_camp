0503b2aa55166d31e1bca94bbb6ec3e389d44bbb
Mikhail Stolbov <mstolbov@gmail.com>
2012-07-03 19:36:03 +0400

    added rake tast invoke to capistrano task

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,6 +21,13 @@ namespace :deploy do
   end
 end
 
+namespace :capi do
+  desc 'invoke rake task. Example: cap capi:task_invoke TASK="db:seed"'
+  task :task_invoke  do
+    run "cd #{deploy_to}/current && bundle exec rake #{ENV['TASK']} RAILS_ENV=#{rails_env}"
+  end
+end
+
 before 'deploy:finalize_update', 'deploy:symlink_db'
 after "deploy:restart", "unicorn:stop"
 after "deploy:update", "deploy:cleanup"
