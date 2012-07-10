cf4d860c149e64c4dc42787303d9e1ee25149358
Mikhail Stolbov <mstolbov@gmail.com>
2012-07-10 19:51:39 +0400

    fixed images path

--- app/uploaders/photo_album/photo_uploader.rb
+++ app/uploaders/photo_album/photo_uploader.rb
@@ -11,7 +11,7 @@ class PhotoAlbum::PhotoUploader < CarrierWave::Uploader::Base
   storage :file
 
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "system/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
